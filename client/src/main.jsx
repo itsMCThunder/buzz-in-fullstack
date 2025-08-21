b2df95001018670268ce5b3979299a54479be475
itsMCThunder <130940477+itsMCThunder@users.noreply.github.com>
2025-08-21 14:26:26 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import App from "./App.jsx";
+import "./index.css"; // ✅ Tailwind + custom styles MUST be imported here
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="bg-animated">
-      <App />
-    </div>
+    <App />
   </React.StrictMode>
 );
