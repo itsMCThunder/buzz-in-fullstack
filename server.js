5b1b3ee539167e2be538e59debff00751a7a55b0
itsMCThunder <130940477+itsMCThunder@users.noreply.github.com>
2025-08-21 14:08:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // ✅ Serve React build (from client/dist instead of client-dist)
 // Serve React build directly from /client/dist
-const clientPath = path.join(__dirname, "../client-dist");
+const clientPath = path.join(__dirname, "client-dist");
 app.use(express.static(clientPath));
 
 app.get("*", (req, res) => {
