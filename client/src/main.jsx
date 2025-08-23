49ac75ef8751337c6ac23f56949b2a0b59e91650
itsMCThunder <130940477+itsMCThunder@users.noreply.github.com>
2025-08-23 08:48:01 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
+// client/src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";  // keep Tailwind/global CSS imported here
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
